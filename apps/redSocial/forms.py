a3e571f1176d2be9fcfe17ebc22d5c52e1f36c66
paola san martin <paolasm93@gmail.com>
2021-06-25 07:20:28 -0400

    isegundo cambio

--- apps/redSocial/forms.py
+++ apps/redSocial/forms.py
@@ -1,21 +1,33 @@
+
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.db.models import fields
-from .models import Post
+from .models import *
 
 class UserRegisterForm(UserCreationForm):
-    nombre = forms.CharField()
-    apellido = forms.CharField()
+    nombre = forms.CharField(label="Nombre del dueño")
+    fecha_nacimiento= forms.DateField()
+    apellido = forms.CharField(label="Nombre de la mascota")
     email = forms.EmailField()
     password1 = forms.CharField(label="Contraseña")
     password2 = forms.CharField(label="Confirmar Contraseña")
 
     class Meta:
         model = User
-        fields = ['nombre','apellido','username', 'email', 'password1', 'password2']
+        fields = ['nombre','apellido', 'fecha_nacimiento', 'email', 'password1', 'password2']
         
 
+
+# class TiendaForm(forms.ModelForm):
+#     nombre_producto = forms.CharField()
+#     descripcion = forms.CharField()
+#     content = forms.CharField(label='publica aqui tu producto')
+
+
+#     class Meta:
+#         model = Tiendas
+#         fields = ['content']
 
 class PostForm(forms.ModelForm):
     content = forms.CharField(label='')
@@ -23,3 +35,5 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['content']
+
+
